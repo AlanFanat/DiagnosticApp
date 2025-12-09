d11b5dd5772ea9a4d0b68133cfdbca8e1ba8ac19
AlanFanat <ID+AlanFanat@users.noreply.github.com>
2025-12-09 16:36:45 +0300

    Initial commit

---
+++ app/src/main/java/com/Fanat/diagnosticapp/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.Fanat.diagnosticapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.Fanat.diagnosticapp.ui.theme.DiagnosticAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContent {
+//            DiagnosticAppTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+//        }
+        setContentView(R.layout.main_activity)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DiagnosticAppTheme {
+        Greeting("Android")
+    }
+}
